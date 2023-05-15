6d932a55772128b4563030f841aa052341fc11d2
Andrey Shorokhov <arshor@rambler.ru>
2023-05-15 19:46:16 +0500

    Add Ingredient Parameterized Type test

---
+++ src/test/java/praktikum/IngredientParamTest.java
@@ -0,0 +1,44 @@
+package praktikum;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class IngredientParamTest {
+
+    private Ingredient ingredient;
+    private final IngredientType type;
+    private final String name;
+    private final float price;
+
+    public IngredientParamTest(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] dataForTest() {
+        return new Object[][] {
+                {IngredientType.SAUCE, "1000islands", 4.99f},
+                {IngredientType.FILLING, "tomato", 9.99f},
+        };
+    }
+
+    @Before
+    public void setUp() {
+        ingredient = new Ingredient(type, name, price);
+    }
+
+    @Test
+    public void paramTest() {
+
+        IngredientType actual = ingredient.getType();
+
+        assertEquals("Не верное значение типа ингредиента", type, actual);
+    }
+}
